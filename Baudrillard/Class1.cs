14acfd78f4fc9e3ae63ae42f2b1105ab42659ff8
Nery Brugnoni <nery.brugnoni@gmail.com>
2018-10-27 14:46:12 -0300

    Add project files.

---
+++ Baudrillard/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baudrillard
+{
+    public class Class1
+    {
+    }
+}
